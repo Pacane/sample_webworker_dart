63e75b4d96bffb62e1286ceac00d083f5a52463f
Joel Trottier-Hebert <iamquebecois@gmail.com>
2023-01-22 11:05:26 -0500

    initial commit

---
+++ android/app/src/main/kotlin/health/braver/MainActivity.kt
@@ -0,0 +1,6 @@
+package health.braver.sample_ww
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:sample_ww/app/app.dart';
+import 'package:sample_ww/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
